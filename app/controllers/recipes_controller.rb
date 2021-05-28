ea74ac78d9c2790a41b8c99c29d86cfd3be4fe6a
vinnyk <ragdolleye@gmail.com>
2021-05-28 10:32:32 -0500

    Adds post/show route for recipes. Also allows user to pass two strings for a better search, without breaking if the user only passes a single variable.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,5 +5,13 @@ class RecipesController < ApplicationController
     response = HTTP.get("https://api.edamam.com/api/food-database/v2/parser?ingr=red%20apple&app_id=#{Rails.application.credentials.food_user_id}&app_key=#{Rails.application.credentials.food_api_key}").body.to_s
     render json: response
   end
+
+  def show
+    string1 = params[:string1] # Takes in first variable
+    string2 = "%20" + params[:string2].to_s  # Allows user to pass a second variable, or not, without breaking code.
+    params[:string2]  ? (string2 = "%20" + params[:string2]) : string2 = "" # Allows user to pass a second variable, or not, without breaking code.
+    response = HTTP.get("https://api.edamam.com/api/food-database/v2/parser?ingr=#{string1}#{string2}&app_id=#{Rails.application.credentials.food_user_id}&app_key=#{Rails.application.credentials.food_api_key}").body.to_s
+    render json: response
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/index" => "recipes#index"
+  post "/index" => "recipes#show" # More appropriate for the input this site expects
 end
